cd99ef15687bd586f14d004f14c3fb1f1ed513a0
Aleksandr-Pod <stdfire@gmail.com>
2022-04-09 22:06:11 +0300

    Update App.jsx
    + PropTypes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,27 +15,27 @@ export class App extends Component {
     // return (this.state.good + this.state.neutral + this.state.bad); // Хардкор
     return Object.values(this.state).reduce((total, el) => total + el, 0); // для любого кол-ва
   }
-  countPositiveFeedbackPercentage = () => {
-    return (this.state.good / this.countTotalFeedback() * 100);
-  }
-  
-  onFeedbackHandle = (evt) => {
+  countPositiveFeedbackPercentage = () => (Math.round(this.state.good / this.countTotalFeedback() * 100));
 
+  onFeedbackHandle = (evt) => {
     const stateName = evt.currentTarget.name;
     this.setState(prev => ({ [stateName]: prev[stateName] + 1 }));
   }
+  btns = () => Object.keys(this.state);
+  
   render() {
+    const {good, neutral, bad} = this.state
     return (
       <div>
         <Section title="Please leave feedback">
-          <FeedbackOptions options={Object.keys(this.state)} onLeaveFeedback={this.onFeedbackHandle}/>
+          <FeedbackOptions options={this.btns()} onLeaveFeedback={this.onFeedbackHandle}/>
         </Section>
         
         <Section title="Statistics">
           {this.countTotalFeedback() ?
-            <Statistics good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
+            <Statistics good={good}
+              neutral={neutral}
+              bad={bad}
               total={this.countTotalFeedback}
               positivePercentage={this.countPositiveFeedbackPercentage} /> :
               <Notification message="There is no feedback"/> }
@@ -47,4 +47,18 @@ export class App extends Component {
 FeedbackOptions.propTypes = {
   options: PropTypes.array,
   onLeaveFeedback: PropTypes.func
+}
+Section.propType = {
+  title: PropTypes.string,
+  children: PropTypes.node
+}
+Statistics.propTypes = {
+  good: PropTypes.string,
+  neutral: PropTypes.string,
+  bad: PropTypes.string,
+  total: PropTypes.func,
+  positivePercentage: PropTypes.func
+}
+Notification.propTypes = {
+  message: PropTypes.string
 }
